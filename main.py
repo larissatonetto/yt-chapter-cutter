ace15fdf374b06e505c8398fcc094df89bc0c3c1
Larissa Tonetto <larissatonetto0@gmail.com>
2023-11-11 19:35:35 -0300

    First Commit

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pytube import YouTube
 from bs4 import BeautifulSoup
 import requests
 import re
+import yt_dlp
 
 pattern = re.compile("\d+:\d+(:\d+)*")
 
@@ -15,6 +16,7 @@ def get_description(link):
     return description
 
 
+# Separa apenas as linhas da descrição que contém um timestamp
 def extract_chapter_timestamps(description):
     desc_lines = description.splitlines()
     filtered_lines = []
@@ -26,21 +28,27 @@ def extract_chapter_timestamps(description):
     return filtered_lines
 
 
+# Retorna uma tupla contendo os int de hora, minuto, segundo e o nome do capítulo
 def separate_time_name(str):
-    out = (str.replace("\t", " ").replace("\u3000", " ")).split()
+    out = (str.replace("\t", " ").replace("\u3000", " ").replace("-", " ")).split()
     time_parts = out[0].split(":")
 
-    return (tuple(map(int, time_parts)), out[1])
+    return (tuple(map(int, time_parts)), out[1:])
 
 
-LINK = ""
+def get_chapter_times(lines):
+    ch_list = []
+    for i in range(0, len(lines)):
+        time, title = separate_time_name(lines[i])
+        start = time
+
+        ch_list.append({"title": title, "start_time": start})
+
+    return ch_list
+
+
+LINK = "https://www.youtube.com/watch?v=0wTf_bbkW2U"
 VIDEO_NAME = "video.mp4"
-
-# Save video frames as chapter previews
-
-if not LINK:
-    print("Video not found.")
-    sys.exit(1)
 
 yt = YouTube(LINK)
 print(yt.title)
@@ -54,27 +62,35 @@ if not os.path.exists(VIDEO_NAME):
     )
     sys.exit(1)
 
-start_time = (0, 0, 0)
-audio = AudioFileClip(VIDEO_NAME)
-for i in range(0, len(lines)):
-    # Pegando o valor do capítulo atual e o tempo que começa o recorte
-    start_time, name = separate_time_name(lines[i])
-    print(start_time, name)
-    end_time = -1
+chapters = get_chapter_times(lines)
+print(chapters)
 
-    # Procurando o final do recorte
-    try:
-        next_line = lines[i + 1]
-        end_time, next_name = separate_time_name(next_line)
-        print("End time: ", end_time)
+for chapter in chapters:
+    print(f"Nome = {chapter['title']}")
 
-    except IndexError:
-        print("Fim da lista")
-        pass
+# start_time = (0, 0, 0)
+# audio = AudioFileClip(VIDEO_NAME)
+# for i in range(0, len(lines)):
+#     # Pegando o valor do capítulo atual e o tempo que começa o recorte
+#     start_time, name = separate_time_name(lines[i])
+#     print(start_time, name)
+#     end_time = -1
 
-    if end_time == -1:
-        sc = audio.subclip(start_time)
-    else:
-        sc = audio.subclip(start_time, end_time)
+#     # Procurando o final do recorte
+#     try:
+#         next_line = lines[i + 1]
+#         end_time, next_name = separate_time_name(next_line)
+#         print("End time: ", end_time)
 
-    sc.write_audiofile("output/" + name + ".wav", nbytes=4, codec="pcm_s16le")
+#     except IndexError:
+#         print("Fim da lista")
+#         pass
+
+#     if end_time == -1:
+#         sc = audio.subclip(start_time)
+#     else:
+#         sc = audio.subclip(start_time, end_time)
+
+#     sc.write_audiofile(
+#         "output/" + yt.title + name + ".wav", nbytes=4, codec="pcm_s16le"
+#     )
